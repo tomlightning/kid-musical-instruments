645e4a4b4063e9045628d1ddc2a73b7a6fe522f7
tomlightning <le.tomer@gmail.com>
2022-12-14 17:38:52 +0200

    shorter flute

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = "/kid-musical-instruments";
 var APP_PREFIX = "music_instru_";
-var VERSION = "version_002";
+var VERSION = "version_003";
 var URLS = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
