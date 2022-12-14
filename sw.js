bea7cf8c03ebf5da9660b9c9c6618cb3ec4dd2b3
tomlightning <le.tomer@gmail.com>
2022-12-14 17:49:10 +0200

    images and next ver

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = "/kid-musical-instruments";
 var APP_PREFIX = "music_instru_";
-var VERSION = "version_003";
+var VERSION = "version_004";
 var URLS = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
